f4921aac50603deaae46c4a0a22ac0e9a0ecac98
Egidijus Gecius <e.gecius@gmail.com>
2018-12-18 09:50:36 +0000

    demo how to cancel a scheduled task

--- app/src/main/java/com/egecius/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/egecius/workmanagerdemo/MainActivity.kt
@@ -4,17 +4,32 @@ import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var uuidOfQueuedRequest : UUID? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupCancelButton()
+    }
 
+    private fun setupCancelButton() {
+        findViewById<Button>(R.id.cancel_work).setOnClickListener {
+            uuidOfQueuedRequest?.let { it1 -> WorkManager.getInstance().cancelWorkById(it1) }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
         demoWorkManager()
     }
 
@@ -31,13 +46,16 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         WorkManager.getInstance().enqueue(request)
+        uuidOfQueuedRequest = request.id
 
         observeWorkerProgress(request)
     }
 
     private fun observeWorkerProgress(request: OneTimeWorkRequest) {
         WorkManager.getInstance().getWorkInfoByIdLiveData(request.id).observe(this, Observer { workInfo ->
-            Log.i("Eg:MainActivity:39", "observeWorkerProgress workInfo $workInfo")
+            val state = workInfo?.state
+            Log.i("Eg:MainActivity:39", "observeWorkerProgress workInfo $state")
+            Toast.makeText(this, "$state", Toast.LENGTH_SHORT).show()
         })
     }
 }
